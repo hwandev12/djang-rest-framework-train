5f4bec5868f2eca79464e15c4ecbe1830d70aa8c
Husan <102412893+hwandev12@users.noreply.github.com>
2022-06-07 13:01:57 +0500

    create posts category and dates

--- coreDrf/posts/models.py
+++ coreDrf/posts/models.py
@@ -1,3 +1,4 @@
+from sre_constants import CATEGORY
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -6,9 +7,18 @@ class UserModel(AbstractUser):
     pass
 
 
+CATEGORY_CHOICES = (
+    ('Dj', 'Django'),
+    ('Py', 'Python')
+)
+
 # create class model here
 class Posts(models.Model):
     title = models.CharField(max_length=100)
+    post_id = models.IntegerField()
+    category = models.TextField(max_length=255, choices=CATEGORY_CHOICES)
+    start_date = models.DateTimeField(auto_now_add=True)
+    change_date = models.DateTimeField(auto_now_add=True)
 
-    def __str_(self):
+    def __str__(self):
         return self.title
--- coreDrf/posts/serializers.py
+++ coreDrf/posts/serializers.py
@@ -5,4 +5,10 @@ from .models import Posts
 class SerializerModel(serializers.ModelSerializer):
     class Meta:
         model = Posts
-        fields = ("__all__")
+        fields = (
+            'title',
+            'post_id',
+            'category',
+            'start_date',
+            'change_date'
+        )
